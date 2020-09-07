3c07b6fc0607b3a942c1b9c75c0d1d1738af9103
mkxmkx <1197993121@qq.com>
2020-09-07 10:50:48 +0800

    chinese prerequisite code

---
+++ chinese_prerequisite_code/model_v1/setup_training.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#python filter_embeddings.py /home/makexin/makexin/Experiment/data/CrowdComp/glove.840B.300d.txt /home/makexin/makexin/Experiment/data/CrowdComp/all_topic_pair_sentences_train_data.json
+python filter_embeddings.py /home/makexin/makexin/Experiment/data/Chinese/sgns.wiki.char.txt /home/makexin/makexin/Experiment/data/Chinese/dataStructure/all_topic_pair_sentences_data.json
